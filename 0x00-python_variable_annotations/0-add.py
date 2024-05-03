de077a605c4e2c9224ec3620bd10db3413f08760
uluchukwu <urealsean12@gmail.com>
2024-05-03 15:05:09 +0100

    README & TASK_0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+# function add that takes a float a and a float b as arguments
+# and returns their sum as a float
+
+from typing import callable, Iterator, Union, Optional
+
+def add(a:float, b:float) -> float:
+    return float(a + b)
