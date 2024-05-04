2c5924a2e2e30f576de562bcb77ac25b072a98d0
uluchukwu <urealsean12@gmail.com>
2024-05-04 09:57:06 +0100

    upadate

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-
 ''' define variables
 
 '''
 
+
 a: int = 1
-
 pi: float = 3.14
-
 i_understand_annotations: bool = True
-
-school: str = "Holberton"
+school: str = 'Holberton'
