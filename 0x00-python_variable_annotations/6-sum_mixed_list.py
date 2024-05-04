8874f9c8f424c9013b2a614347e97ccc583a098a
uluchukwu <urealsean12@gmail.com>
2024-05-04 11:33:49 +0100

    task6.

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''task 6's module
+'''
+
+
+def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+    '''gives the sum of float and int in a list
+    '''
+    return float(sum(mxd_lst))
