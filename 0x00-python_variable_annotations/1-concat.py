5403f4922971d0d4641ef44b1ac655db64737703
uluchukwu <urealsean12@gmail.com>
2024-05-04 04:04:37 +0100

    task-1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 '''task-1 module
-
+    A fuction that concatenate two strings str1 & str2.
 '''
 
 def concat(str1: str, str2: str) -> str:
     '''concatenate two strings
 
     '''
-    
+
     return str1+str2
